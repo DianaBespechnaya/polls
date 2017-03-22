85463edc3ab8fad8f97202460651057039709432
DianaBespechnaya <dianabespechnaya@yandex.ru>
2017-03-22 21:41:01 +0300

    login

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,9 @@ from django.contrib import admin
 from polls import views
 
 urlpatterns = [
+    url(r'^login/$', 'django.contrib.auth.views.login', {
+        'template_name': 'admin\login.html'
+    }, name='login'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('polls.polls_list')),
+    url(r'', include('polls.urls')),
     ]
