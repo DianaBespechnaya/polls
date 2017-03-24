45e471e357fb7d32b11205b435e06463ea70fdb9
DianaBespechnaya <dianabespechnaya@yandex.ru>
2017-03-24 22:50:49 +0300

    posts

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Post
+from django.utils import timezone
+
 
 # Create your views here.
 def polls_list(request):
@@ -7,4 +10,5 @@ def polls_list(request):
 
 
 def account(request):
-    return render(request, 'admin/account.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'admin/account.html', {'posts': posts})
