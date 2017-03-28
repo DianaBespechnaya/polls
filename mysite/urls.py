8ee2802129445ef667dcf1c451d443bd3146a4d3
DianaBespechnaya <dianabespechnaya@yandex.ru>
2017-03-28 23:31:06 +0300

    верстка + различные посты

--- mysite/urls.py
+++ mysite/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^logout/$', 'django.contrib.auth.views.logout',{
         'template_name': 'admin\home.html'
     }, name='logout'),
+    url(r'^signup/', views.sign_up, name='signup'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('polls.urls')),
     ]
--- polls/views.py
+++ polls/views.py
@@ -3,17 +3,29 @@ from django.contrib.auth.decorators import login_required
 from .models import Post
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.contrib.auth.models import User
+
+from django.contrib.auth.middleware import AuthenticationMiddleware
+from django.http import  HttpRequest
 
 
 # Create your views here.
 def polls_list(request):
     return render(request, 'admin/home.html', {})
 
+def sign_up(request):
+    return render(request, 'admin/signup.html', {})
 
 def account(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    me = request.user
+    posts = Post.objects.filter(author = me, published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'admin/account.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'admin/post.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'admin/post_edit.html', {'form': form})
