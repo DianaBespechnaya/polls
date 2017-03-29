414935f9270bef5a4f4c9c84955ab6fd5b9226d5
DianaBespechnaya <dianabespechnaya@yandex.ru>
2017-03-29 21:24:13 +0300

    Adding a new post

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.contrib.auth.models import User
+from django.shortcuts import redirect
 
 from django.contrib.auth.middleware import AuthenticationMiddleware
 from django.http import  HttpRequest
@@ -26,6 +27,16 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'admin/post.html', {'post': post})
 
+
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'admin/post_edit.html', {'form': form})
